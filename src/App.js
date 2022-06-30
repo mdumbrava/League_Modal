1c8faf030ec1104f53631cb7c27877ae6f9ea743
mdumbrava <mihaela.dmbrv@gmail.com>
2022-06-29 20:16:47 -0400

    folders

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import MainPageRequest from "./MainPageRequests/MainPageRequests";
 
 function App() {
   return (
     <div className="App">
       <h1>HELLO!!!</h1>
+      <MainPageRequest />
     </div>
   );
 }
---
+++ src/MainPageRequests/MainPageRequests.js
@@ -0,0 +1,8 @@
+const MainPageRequest = () => {
+  return (
+    <div>
+      <h1>Request</h1>
+    </div>
+  );
+};
+export default MainPageRequest;
