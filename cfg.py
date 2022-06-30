b98b7d40c47a8485620eb3d60afcac89bbe24482
mdumbrava <mihaela.dmbrv@gmail.com>
2022-06-29 20:14:45 -0400

    add flask

---
+++ cfg.py
@@ -0,0 +1,16 @@
+IP            = '0.0.0.0'
+PORT          = 9000
+LST_MODE      = [
+                'development',
+                'production'
+                ]
+MODE          = LST_MODE[0]
+DEBUG         = True
+STATIC_FOLDER = './build'
+STATIC_PATH   = '/'
+NICE_LOG      = True
+HIDE_STATIC   = True
+LOG_FLASK     = False
+PROXY         = False
+PAGE_SIZE     = 2
+COMM_FILE     = './comments.txt'
