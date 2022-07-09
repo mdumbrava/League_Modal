5f6d33b6e20cb1eea07d3fb8dd43c5130e468813
mdumbrava <mihaela.dmbrv@gmail.com>
2022-07-09 15:18:08 -0400

    tab data api

--- run.py
+++ run.py
@@ -1,9 +1,14 @@
 import json, time, math, logging
-from   cfg       import *
-from   flask     import Flask, redirect, request, render_template, url_for
-from   datetime  import datetime
-from   flask     import make_response
-from   functools import wraps, update_wrapper
+
+from cfg                      import *
+from flask                    import Flask, redirect, request, render_template, url_for
+from flask_sqlalchemy         import SQLAlchemy
+from flask_admin              import Admin, AdminIndexView
+from flask_admin.contrib.sqla import ModelView
+from datetime                 import datetime
+from flask                    import make_response
+from functools                import wraps, update_wrapper
+
 
 app = Flask(
             __name__, 
@@ -11,14 +16,103 @@ app = Flask(
             static_url_path = STATIC_PATH, 
             )
 
+
+app.config['SECRET_KEY'] = 'ABCDEFGIJKLMNOP'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///league_db.sql'
+db = SQLAlchemy(app) 
+class Champion(db.Model):
+    id   = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50))
+    pic  = db.Column(db.String(100))
+class About(db.Model):
+    id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_ch = db.Column(db.Integer)
+    title = db.Column(db.String(50))
+    pos   = db.Column(db.String(50))
+    clas  = db.Column(db.String(50))
+    desc  = db.Column(db.String(2000))
+    pic   = db.Column(db.String(100))
+class Abilities(db.Model):
+    id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_ch = db.Column(db.Integer)
+    icon  = db.Column(db.String(100))
+    text  = db.Column(db.String(2000))
+    title = db.Column(db.String(100))
+class Cosmetics(db.Model):
+    id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    id_ch = db.Column(db.Integer)
+    pic   = db.Column(db.String(100))
+# db.drop_all()
+# db.create_all()
+
+def check_admin():
+    return ADMIN_ENABLE
+
+
+# Flask Admin Config
+# this will allow Champion table
+class ChampionView(ModelView):
+    page_size            = 100
+    can_delete           = True
+    can_view_details     = True
+    column_list          = ['id', 'name', 'pic']
+    column_editable_list = ['name', 'pic']
+    column_exclude_list  = []
+    def is_accessible(self):
+        return check_admin()
+class AboutView(ModelView):
+    page_size            = 100
+    can_delete           = True
+    can_view_details     = True
+    column_list          = ['id', 'id_ch', 'title', 'pos', 'clas', 'desc', 'pic']
+    column_editable_list = ['id_ch', 'title', 'pos', 'clas', 'desc', 'pic']
+    column_exclude_list  = []
+    def is_accessible(self):
+        return check_admin()
+class AbilitiesView(ModelView):
+    page_size            = 100
+    can_delete           = True
+    can_view_details     = True
+    column_list          = ['id', 'id_ch', 'icon', 'text', 'title']
+    column_editable_list = ['id_ch', 'icon', 'text', 'title']
+    column_exclude_list  = []
+    def is_accessible(self):
+        return check_admin()
+class CosmeticsView(ModelView):
+    page_size            = 100
+    can_delete           = True
+    can_view_details     = True
+    column_list          = ['id', 'id_ch', 'pic']
+    column_editable_list = ['id_ch', 'pic']
+    column_exclude_list  = []
+    def is_accessible(self):
+        return check_admin()
+
+
+class MyAdminView(AdminIndexView):
+    def is_accessible(self):
+        return False
+        # return check_admin()
+
+admin = Admin(app, index_view=MyAdminView(), template_mode='bootstrap4', name='League Admin')
+admin.add_view(ChampionView (Champion    , db.session))
+admin.add_view(AboutView    (About       , db.session))
+admin.add_view(AbilitiesView(Abilities   , db.session))
+admin.add_view(CosmeticsView(Cosmetics   , db.session))
+
+
+
 if not LOG_FLASK:
     log = logging.getLogger('werkzeug')
     log.disabled = True
+
 
 # this helps with caching
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 43200
 
 app.config['ENV'] = MODE
+
 
 @app.route('/')
 def index():
@@ -26,19 +120,49 @@ def index():
 
 @app.route('/apileague/get_main')
 def get_commts():
-    return      '''
-                [{"id": 1, "name": "Caitlin", "pic": "/static/media/main/caitlin.jpg"}, 
-                 {"id": 2, "name": "Diana", "pic": "/static/media/main/diana.jpg"},
-                 {"id": 3, "name": "Morgana", "pic": "/static/media/main/morgana.jpg"}, 
-                 {"id": 4, "name": "Nami", "pic": "/static/media/main/nami.jpg"}]
-                '''
+    data = Champion.query.all()
+    lst_data =[]
+    for x in data:
+        obj_champ = {}
+        obj_champ['id'  ] = x.id
+        obj_champ['name'] = x.name
+        obj_champ['pic' ] = x.pic
+        lst_data.append(obj_champ)
+    return json.dumps(lst_data)
 
 @app.route('/apileague/get_champion/<id>')
 def get_champion(id):
     try:
-        val_id = int(id) - 1
-        lst_champions = ['Caitlin', 'Diana', 'Morgana', 'Nami']
-        rv = lst_champions[val_id]
+        val_id = int(id)
+        data_about  = About.query.filter_by(id_ch = val_id).first()
+        dct_about = {}
+        dct_about['id']          = data_about.id
+        dct_about['title']       = data_about.title
+        dct_about['position']    = data_about.pos
+        dct_about['class']       = data_about.clas
+        dct_about['description'] = data_about.desc
+        dct_about['pic']         = data_about.pic
+        data_abilit = Abilities.query.filter_by(id_ch = val_id).all()
+        lst_abilit = []
+        for x in data_abilit:
+            dct_abil = {}
+            dct_abil['id']    = x.id
+            dct_abil['icon']  = x.icon
+            dct_abil['text']  = x.text
+            dct_abil['title'] = x.title
+            lst_abilit.append(dct_abil)
+        data_cosmet = Cosmetics.query.filter_by(id_ch = val_id).all()
+        lst_cosm = []
+        for x in data_cosmet:
+            dct_cosm = {}
+            dct_cosm['id']  = x.id
+            dct_cosm['pic'] = x.pic
+            lst_cosm.append(dct_cosm)
+        dct_data = {}
+        dct_data['about']     = dct_about
+        dct_data['abilities'] = lst_abilit
+        dct_data['cosmetics'] = lst_cosm
+        rv = json.dumps(dct_data)
     except:
         rv = 'None'
     return  rv
